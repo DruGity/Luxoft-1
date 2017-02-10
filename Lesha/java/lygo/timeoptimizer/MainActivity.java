2baa66ae4fa21ba916625342820388759e009a7c
Aleksey Chudak <druggy3392@mail.ru>
2017-02-10 22:45:33 +0200

    .R

--- Lesha/java/lygo/timeoptimizer/MainActivity.java
+++ Lesha/java/lygo/timeoptimizer/MainActivity.java
@@ -1,7 +1,10 @@
 package lygo.timeoptimizer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        kek = (Button) findViewById(R.id.angry_btn);
+        kek.setOnClickListener(this);
+
     }
 }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnActTwo:
+                Intent intent = new Intent(this, AboutUs.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
