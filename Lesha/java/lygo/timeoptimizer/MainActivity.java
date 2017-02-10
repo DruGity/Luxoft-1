4daf57cd7ba1557777894ccce51f99c5a1e68fce
Aleksey Chudak <druggy3392@mail.ru>
2017-02-10 23:26:41 +0200

    w/o style

--- Lesha/java/lygo/timeoptimizer/MainActivity.java
+++ Lesha/java/lygo/timeoptimizer/MainActivity.java
@@ -3,30 +3,35 @@ package lygo.timeoptimizer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
+    Button kek;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        kek = (Button) findViewById(R.id.angry_btn);
+        Button kek = (Button) findViewById(R.id.kek);
         kek.setOnClickListener(this);
 
     }
-}
 
-    @Override
+
+
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btnActTwo:
-                Intent intent = new Intent(this, AboutUs.class);
+            case R.id.kek:
+                Intent intent = new Intent(this, AboutUs.class);  // ссылку на какой класс будет переходить по кнопке
                 startActivity(intent);
                 break;
             default:
                 break;
         }
     }
+
+}
